75df31fb1d07ba24c40e6863f7f2452fc18e2c1a
Michael Jenkins <michael.jenkins@disney.com>
2016-10-19 16:05:51 -0700

    Create host init script

---
+++ host_init.py
@@ -0,0 +1,20 @@
+import json
+import platform
+import ConfigParser
+from cm_api.api_client import ApiResource
+
+hostname = platform.uname()[1]
+
+config = ConfigParser.ConfigParser()
+config.read('config.txt')
+
+manager  = config.get('api','manager')
+port     = config.get('api','port')
+username = config.get('api','username')
+password = config.get('api','password')
+
+api = ApiResource(manager, port, username, password)
+
+for h in api.get_all_hosts():
+    if hostname == h.hostname:
+        # init stuff goes here
